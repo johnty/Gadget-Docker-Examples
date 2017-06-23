4c26a5a600a8ca5d036559495b6ffc2bbd08b5ca
Lara <heavyhearts@gmail.com>
2017-06-23 16:39:30 -0700

    blink example

---
+++ blink/blink.py
@@ -0,0 +1,22 @@
+import CHIP_IO.GPIO as GPIO
+from time import sleep
+
+
+ledPin = "CSID0"
+
+GPIO.setup(ledPin, GPIO.OUT)
+
+try:
+	while True:
+		GPIO.output(ledPin, True)
+		sleep(1)
+		GPIO.output(ledPin, False)
+		sleep(1)
+	
+#exit with CTRL+C			
+except KeyboardInterrupt:
+	print("exiting")
+	
+#unexport GPIOs upon exiting      
+finally:
+	GPIO.cleanup() 
